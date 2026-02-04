380f95b6f1901d76a8e349789144447d2a613fd5
itxharsha46 <harshahavaldar09@gmail.com>
2026-02-04 20:06:12 +0530

    Update backend to match frontend rules

--- backend/main.py
+++ backend/main.py
@@ -116,7 +116,7 @@ async def process_document(file: UploadFile = File(...)):
         status_code=200, content={"message": "Document processed successfully in batches.", "session_id": session_id}
     )
 
-@app.post("/query   ", tags=["Question Answering"])
+@app.post("/query", tags=["Question Answering"])
 async def query_document(request: QueryRequest):
     session_id = request.session_id
     question = request.question
